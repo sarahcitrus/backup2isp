62fdc9ce4adcec9307c313db52b153a81c8b40b7
Sarah Citrus <sarahcitrus@mail.ru>
2011-06-26 22:17:30 +0100

    Added inotify support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import pyinotify, json, os, commands, backup, sys, signal
+import pyinotify, json, os, commands, backup, sys
 
 wm = pyinotify.WatchManager()  # Watch Manager
 mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_CLOSE_WRITE  # watched events
@@ -12,24 +12,27 @@ class EventHandler(pyinotify.ProcessEvent):
         if event.dir:
 	  localwatches.append(wm2.add_watch(event.pathname, mask, rec=True));
 	  print "create", event.pathname
+	  backup.uploadMultipleFiles(event.pathname)
 
     def process_IN_DELETE(self, event):
 	print "remove", event.pathname
+	backup.deleteFileByPath(event.pathname)
         
     def process_IN_CLOSE_WRITE(self, event):
+	print "close write"
 	if os.path.exists(event.pathname):
 	  print "modify", event.pathname
+	  backup.uploadMultipleFiles(event.pathname)
 	else:
 	  print "remove", event.pathname
+	  backup.deleteFileByPath(event.pathname)
 
-def signal_handler(signal, frame):
-        print "Quitting"
-        sys.exit(0)
-        
-notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
-wdd = wm.add_watch('/tmp/testsync', pyinotify.IN_CLOSE_WRITE)
-
-signal.signal(signal.SIGINT, signal_handler)
-print 'Listening for changes, Ctrl+C to abort'
-signal.pause()
-notifier.stop()
+notifier = pyinotify.Notifier(wm, EventHandler())
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_CLOSE_WRITE 
+wdd = wm.add_watch('/tmp/testsync', mask)
+try:
+  notifier.loop()
+except KeyboardInterrupt:
+  print "Exiting"
+  notifier.stop()
+  sys.exit(1)
